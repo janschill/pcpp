cc4c67ca3688ff1e9de30a86d7f6fcc748b6c1c8
Jan Schill <janschill@me.com>
2019-09-04 18:14:43 +0200

    Update on on 1.4#3 and file restructure

--- 01-exercise/hand_in/src/src/TestCountPrimes.java
+++ 01-exercise/hand_in/src/src/TestCountPrimes.java
@@ -1,4 +1,3 @@
-package testfiles;
 // Week 1
 
 // Counting primes, using multiple threads for better performance.
@@ -9,8 +8,8 @@ public class TestCountPrimes {
 	public static void main(String[] args) {
 		final int range = 50_000_000;
 		double start = System.currentTimeMillis();
-		System.out.printf("Sequential result: %10d%n%n", countSequential(range));
-//      System.out.printf("Parallel2  result: %10d%n%n", countParallel2(range/2));
+//		System.out.printf("Sequential result: %10d%n%n", countSequential(range));
+      System.out.printf("Parallel2  result: %10d%n%n", countParallel2(range/2));
 //		System.out.printf("Parallel4  result: %10d%n%n", countParallelN(range, 4));
 //		System.out.printf("Parallel10 result: %10d%n%n", countParallelN(range, 10));
 		double end = System.currentTimeMillis();
@@ -86,7 +85,7 @@ public class TestCountPrimes {
 class LongCounter {
 	private long count = 0;
 
-	public synchronized void increment() {
+	public  void increment() {
 		count = count + 1;
 	}
 
